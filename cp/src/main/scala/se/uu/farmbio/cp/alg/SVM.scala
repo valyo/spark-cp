4e8252c71129b2574bd0f45d2ba2576ecb4762e3
Valentin Georgiev <valentin.georgiev@farmbio.uu.se>
2015-12-07 16:56:48 +0100

    Implement LBFGS instead of SVMWithSGD

--- cp/src/main/scala/se/uu/farmbio/cp/alg/SVM.scala
+++ cp/src/main/scala/se/uu/farmbio/cp/alg/SVM.scala
@@ -1,8 +1,13 @@
 package se.uu.farmbio.cp.alg
 
-import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.classification.SVMModel
 import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.optimization.LBFGS
+import org.apache.spark.mllib.optimization.LogisticGradient
+import org.apache.spark.mllib.optimization.SquaredL2Updater
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
 
 import se.uu.farmbio.cp.UnderlyingAlgorithm
@@ -12,9 +17,28 @@ object SVM {
   def trainingProcedure(
       input: RDD[LabeledPoint], 
       numIterations: Int): (Vector => Double) = {
+    val numFeatures = input.take(1)(0).features.size
+    val training = input.map(x => (x.label, MLUtils.appendBias(x.features))).cache()
+    //Configuration
+    val numCorrections = 10
+    val convergenceTol = 1e-4
+    val maxNumIterations = 20
+    val regParam = 0.1
+    val initialWeightsWithIntercept = Vectors.dense(new Array[Double](numFeatures + 1))
+
+    val (weightsWithIntercept, loss) = LBFGS.runLBFGS(
+      training,
+      new LogisticGradient(),
+      new SquaredL2Updater(),
+      numCorrections,
+      convergenceTol,
+      maxNumIterations,
+      regParam,
+      initialWeightsWithIntercept)
     //Training
-//    val remappedInput = input.map(x => new LabeledPoint((x.label * 2) - 1, x.features))
-    val model = SVMWithSGD.train(input,numIterations)
+      val model = new SVMModel(
+        Vectors.dense(weightsWithIntercept.toArray.slice(0, weightsWithIntercept.size - 1)),
+        weightsWithIntercept(weightsWithIntercept.size - 1))
     model.predict
   }
 }
